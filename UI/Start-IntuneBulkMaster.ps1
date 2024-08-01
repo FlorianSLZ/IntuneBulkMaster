60bbb956b89bdf41c49d8e9e3770190af319cb2f
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-01 13:15:09 +0200

    removed old drafts

--- UI/Start-IntuneBulkMaster.ps1
+++
@@ -1,9 +0,0 @@
-$Button1_Click = {
-}
-$Form1_Load = {
-}
-$PictureBox1_Click = {
-}
-Add-Type -AssemblyName System.Windows.Forms
-. (Join-Path $PSScriptRoot 'start-intunebulkmaster.designer.ps1')
-$Form1.ShowDialog()
