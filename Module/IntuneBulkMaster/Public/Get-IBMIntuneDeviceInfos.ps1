c34642eade1f0dfebb30605ac457c043fe70bebf
Florian Salzmann <38555854+FlorianSLZ@users.noreply.github.com>
2024-08-07 14:06:22 +0200

    Added support for retrieving devices by serial number(s)

--- Module/IntuneBulkMaster/Public/Get-IBMIntuneDeviceInfos.ps1
+++ Module/IntuneBulkMaster/Public/Get-IBMIntuneDeviceInfos.ps1
@@ -20,22 +20,29 @@ function Get-IBMIntuneDeviceInfos {
             - Added support for nested group memberships
             - Added support for Intune device IDs or all Info output
         - 2024-08-06: 1.2 Added support for passing multiple OS types
+        - 2024-08-07: 1.3 Added support for retrieving devices by serial number(s)
         
     #>
 
     param (
-        [parameter(Mandatory = $false, HelpMessage = "Specify the ID of the individual device to retrieve.")]
-        [string]$DeviceId,
+        [parameter(Mandatory = $false, HelpMessage = "Retrieve all information about the devices.")]
+        [switch]$AllDeviceInfo,
+
+        [parameter(Mandatory = $false, HelpMessage = "Specify the name of the individual device to retrieve.")]
+        [string]$DeviceName,
         
         [parameter(Mandatory = $false, HelpMessage = "Specify the name of the group to which the devices belong.")]
         [string]$GroupName,
         
-        [parameter(Mandatory = $false, HelpMessage = "Specify the name of the individual device to retrieve.")]
-        [string]$DeviceName,
-        
         [parameter(Mandatory = $false, HelpMessage = "Specify the operating system(s) of the devices to retrieve. For example, 'Windows' and/or 'iOS'.")]
         [ValidateSet("Windows","macOS","iOS","iPadOS","Android","Linux (Ubuntu)","")]
         [string[]]$OS,
+
+        [parameter(Mandatory = $false, HelpMessage = "Specify the serial number of the device to retrieve.")]
+        [string]$Serialnumber,
+
+        [parameter(Mandatory = $false, HelpMessage = "Specify the serial numbers of the devices to retrieve.")]
+        [string]$Serialnumbers,
         
         [parameter(Mandatory = $false, HelpMessage = "Retrieve all devices managed by Intune.")]
         [switch]$AllDevices,
@@ -46,8 +53,10 @@ function Get-IBMIntuneDeviceInfos {
         [parameter(Mandatory = $false, HelpMessage = "Select a specific group of devices interactively to retrieve.")]
         [switch]$SelectGroup,
 
-        [parameter(Mandatory = $false, HelpMessage = "Retrieve all information about the devices.")]
-        [switch]$AllDeviceInfo
+        [parameter(Mandatory = $false, HelpMessage = "Specify the ID of the individual device to retrieve.")]
+        [string]$DeviceId
+
+        
     )
 
     # Function to recursively get all device IDs from a group, including nested groups
@@ -76,6 +85,7 @@ function Get-IBMIntuneDeviceInfos {
     }
 
     $filter = $null
+    $IntuneDeviceInfos = $null
 
     if($SelectGroup){
         $GroupsAll = Invoke-IBMPagingRequest -URI "https://graph.microsoft.com/v1.0/groups"
@@ -92,22 +102,15 @@ function Get-IBMIntuneDeviceInfos {
             $EntraDeviceIds = Get-GroupMemberTypeDevice -groupId $groupId
 
             # Map Entra device IDs to Intune managed device IDs
-            $IntuneDevices = @()
+            $IntuneDeviceInfos = @()
             foreach ($EntraDeviceId in $EntraDeviceIds) {
                 $managedDevice = Invoke-IBMPagingRequest -Uri "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=azureADDeviceId eq '$EntraDeviceId'" 
                 if ($managedDevice) {
-                    $IntuneDevices += $managedDevice[0]
+                    $IntuneDeviceInfos += $managedDevice[0]
                 }
             }
-            if($AllDeviceInfo){
-                return $IntuneDevices
-            }else{
-               return $IntuneDevices.id 
-            }
-            break
-
         } else {
-            Write-Output "Group not found."
+            Write-Output "Group ""$GroupName"" not found."
             break
         }
     } elseif ($DeviceName) {
@@ -117,7 +120,27 @@ function Get-IBMIntuneDeviceInfos {
         $osFilters = $OS | ForEach-Object { "operatingSystem eq '$_'" }
         $filter = "(" + ($osFilters -join " or ") + ")"
 
-    } else {
+    } elseif($Serialnumber){
+        $filter = "serialNumber eq '$Serialnumber'"
+    } elseif($Serialnumbers){
+        $serials = $Serialnumbers -split ","
+        $serialFilters = $serials | ForEach-Object { "serialNumber eq '$_'" }
+        $filter = "(" + ($serialFilters -join " or ") + ")"
+
+        $Serialnumbers = $Serialnumbers.Where({ $_ -ne "" })
+
+        $batchingParams = @{
+            "Objects2Process"       = $Serialnumbers
+            "ActionURI"             = "deviceManagement/managedDevices?`$filter=serialNumber eq '{0}'"
+            "Method"                = "GET"
+            "GraphVersion"          = "beta"
+            "BodySingle"            = @{}
+            "ActionTitle"           = "Get Device by Serial Number"
+        } 
+        $IntuneDeviceInfos = Invoke-IBMGrapAPIBatching @batchingParams
+
+
+    }else {
         $filter = $null
     }
 
@@ -127,19 +150,22 @@ function Get-IBMIntuneDeviceInfos {
     } elseif($DeviceId){
         $uri += "/$DeviceId"
         $singleDevice = Invoke-MgGraphRequest -Uri $uri -Method GET
-        return $singleDevice.id
+        return $singleDevice
         break
     }
 
-    $DeviceInfos = Invoke-IBMPagingRequest -Uri $uri
+    if($IntuneDeviceInfos){
+        $IntuneDeviceInfos = Invoke-IBMPagingRequest -Uri $uri
+    }
+
 
     if($SelectDevices){
-        $DeviceInfos = ($DeviceInfos | Out-GridView -PassThru -Title "Select Devices")
+        $IntuneDeviceInfos = ($IntuneDeviceInfos | Out-GridView -PassThru -Title "Select Devices")
     }
 
     if($AllDeviceInfo){
-        return $DeviceInfos
+        return $IntuneDeviceInfos
     }else{
-       return $DeviceInfos.id 
+       return $IntuneDeviceInfos.id 
     }
 }
